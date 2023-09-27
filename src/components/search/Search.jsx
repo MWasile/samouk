50e5a44bdd5a30410ba5093d3c4095ec4d3581b2
MWasile <maaciej.wasilewski@gmail.com>
2023-09-27 08:22:08 +0200

    search component with proxy vite configuration"

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,6 +1,24 @@
-function Search(props) {
+import {useState} from "react";
+import axios from "axios";
+
+function Search() {
+    const [offer, setOffer] = useState("");
+
+    async function getOffers() {
+        return axios.get("http://localhost:3000/offers");
+    }
+
     return (
-        <div> Search </div>
+        <div>
+            <input
+                type="text"
+                placeholder={"Szukaj ofert!"}
+                onChange={(e) => setOffer(e.target.value)}
+            />
+            <section>
+                { offer }
+            </section>
+        </div>
     );
 }
 
