e0397aec88b10e52b75f80abdb0cf6af2b6070a0
MWasile <maaciej.wasilewski@gmail.com>
2023-09-27 08:33:54 +0200

    get data from rest api

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,21 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 function Search() {
     const [offer, setOffer] = useState("");
+    const [offers, setOffers] = useState([]);
+
+    useEffect(() => {
+        getOffers().then(({data}) => {
+            setOffers(data);
+        });
+    }, []);
 
     async function getOffers() {
-        return axios.get("http://localhost:3000/offers");
+        return axios.get("api/v1/offers");
+    }
+
+    function searchOffers() {
     }
 
     return (
@@ -16,7 +26,15 @@ function Search() {
                 onChange={(e) => setOffer(e.target.value)}
             />
             <section>
-                { offer }
+                {offers.length === 0 ?
+                    (<div>Brak ofert</div>)
+                    :
+                    (
+                        offer.map((offer) => (
+                            <div key={offer.id}>{offer.title}</div>)
+                        )
+                    )
+                }
             </section>
         </div>
     );
